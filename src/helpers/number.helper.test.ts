ae8215811793a078a8a331169902833492640145
Seongil lee <modolee.si@gmail.com>
2022-04-10 02:15:59 +0900

    isNumeric 함수 버그 수정
    - isNaN을 !isNaN으로 수정
    - 테스트 코드 추가

---
+++ src/helpers/number.helper.test.ts
@@ -0,0 +1,16 @@
+import { isNumeric } from './number.helper';
+
+describe('Number Helper Test', () => {
+  test('isNumeric', () => {
+    // GIVEN
+    const issueNumber = '2';
+
+    // WHEN
+    const result = isNumeric(issueNumber);
+
+    console.log({ result });
+
+    // THEN
+    expect(result).toBeTruthy();
+  });
+});
--- src/helpers/number.helper.ts
+++ src/helpers/number.helper.ts
@@ -2,5 +2,5 @@ export const isNumeric = (value: number | string | undefined): boolean => {
   if (typeof value === 'undefined') return false;
 
   const isTypeString = typeof value === 'string';
-  return isNaN(isTypeString ? parseInt(value) : value);
+  return !isNaN(isTypeString ? parseInt(value) : value);
 };
